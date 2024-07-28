fdb306dbd23ce363d40b3b7eac6313505f525c0f
Ivan Razmyslov <76975247+Reable@users.noreply.github.com>
2024-07-29 00:41:40 +0300

    delete

--- server/js/bun/database/db.ts
+++ server/js/bun/database/db.ts
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 import * as schema from '../schemas/schema.ts';
 
 export const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST         ?? "127.0.0.1",
+    user: process.env.DB_USER         ?? "root",
+    password: process.env.DB_PASSWORD ?? "root",
+    database: process.env.DB_NAME     ?? "bun",
     multipleStatements: true,
 });
 export const db = drizzle(connection, {
